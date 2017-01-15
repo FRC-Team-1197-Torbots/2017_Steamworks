0425cff1052bf530a64483acc0e47fe31f66060b
Masaki Asanuma <masakiasanuma.pr@gmail.com>
2017-01-14 19:38:16 -0800

    Basic drive and shift code for proto bot.

--- src/org/usfirst/frc/team1197/robot/Robot.java
+++ src/org/usfirst/frc/team1197/robot/Robot.java
@@ -1,16 +1,31 @@
 package org.usfirst.frc.team1197.robot;
 
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.RobotDrive;
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot {
+	private Compressor compressor;
+	private CANTalon rightTalon1;
+	private CANTalon rightTalon2;
+	private CANTalon leftTalon1;
+	private CANTalon leftTalon2;
+	private Solenoid solenoid;
+	private Joystick stick;
+	private RobotDrive robot;
 
     public Robot() {
-
+    	compressor = new Compressor();
+    	rightTalon1 = new CANTalon(2); //right master
+    	rightTalon2 = new CANTalon(3);
+    	leftTalon1 = new CANTalon(4); //left master
+    	leftTalon2 = new CANTalon(6);
+    	stick = new Joystick(0);
+    	solenoid = new Solenoid(1);
+    	robot = new RobotDrive(leftTalon1, leftTalon2, rightTalon1, rightTalon2);
     }
     
     public void robotInit() {
@@ -22,14 +37,20 @@ public class Robot extends SampleRobot {
     }
 
     public void operatorControl() {
-    	
-    	
-  
-    	
-    	
-
+    	while(isEnabled()){
+    		robot.arcadeDrive(stick);
+    		if(stick.getRawButton(1)){
+    			solenoid.set(true);
+    		}
+    		if(stick.getRawButton(2)){
+    			solenoid.set(false);
+    		}
+    	}
     }
 
     public void test() {
+    	while(isEnabled()){
+    		compressor.start();
+    	}
     }
 }

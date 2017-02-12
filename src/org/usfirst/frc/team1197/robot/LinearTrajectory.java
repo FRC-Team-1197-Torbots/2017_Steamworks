9cb8991e5a97ef16a653f8c5fb441adf7376bf40
Masaki Asanuma <masakiasanuma.pr@gmail.com>
2017-02-12 13:57:48 -0800

    Removed old lookup methods from linear and pivot trajectory.

--- src/org/usfirst/frc/team1197/robot/LinearTrajectory.java
+++ src/org/usfirst/frc/team1197/robot/LinearTrajectory.java
@@ -7,28 +7,6 @@ public class LinearTrajectory extends TorTrajectory{
 		build(goal_pos, max_vel, max_acc, max_jerk, translation);
 	}
 	
-	public double lookUpPosition(long t){
-		int i = time.indexOf(t);
-		if(i == -1){
-			return goal_pos;
-		}
-		return translation.get(i).pos;
-	}
-	public double lookUpVelocity(long t){
-		int i = time.indexOf(t);
-		if(i == -1){
-			return 0.0;
-		}
-		return translation.get(i).vel;
-	}
-	public double lookUpAcceleration(long t){
-		int i = time.indexOf(t);
-		if(i == -1){
-			return 0.0;
-		}
-		return translation.get(i).acc;
-	}
-	
 	public double lookUpOmega(long t){
 		return 0;
 	}
--- src/org/usfirst/frc/team1197/robot/PivotTrajectory.java
+++ src/org/usfirst/frc/team1197/robot/PivotTrajectory.java
@@ -7,28 +7,6 @@ public class PivotTrajectory extends TorTrajectory {
 		build(goal_head, max_omg, max_alf, max_jeta, rotation);
 	}
 	
-	public double lookUpHeading(long t){
-		int i = time.indexOf(t);
-		if(i == -1){
-			return goal_head;
-		}
-		return rotation.get(i).pos;
-	}
-	public double lookUpOmega(long t){
-		int i = time.indexOf(t);
-		if(i == -1){
-			return 0.0;
-		}
-		return rotation.get(i).vel;
-	}
-	public double lookUpAlpha(long t){
-		int i = time.indexOf(t);
-		if(i == -1){
-			return 0.0;
-		}
-		return rotation.get(i).acc;
-	}
-	
 	public double lookUpPosition(long t){
 		return 0;
 	}

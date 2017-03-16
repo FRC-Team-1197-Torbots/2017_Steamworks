c4e4c710880e030c0daa22114207d11e46ce18be
Masaki Asanuma <masakiasanuma.pr@gmail.com>
2017-03-16 01:41:45 -0700

    Finished DriveControllerTest sequence.

--- src/org/usfirst/frc/team1197/robot/test/DriveControllerTest.java
+++ src/org/usfirst/frc/team1197/robot/test/DriveControllerTest.java
@@ -9,7 +9,8 @@ import org.usfirst.frc.team1197.robot.TorTrajectory;
 public class DriveControllerTest extends Test{
 	
 	private enum TestPhase {
-		IDLE, BEGIN, FORWARDTRAJ, FORWARDSTALL, BACKWARDTRAJ, RIGHTTRAJ, LEFTTRAJ, COMPLETE
+		IDLE, BEGIN, FORWARDTRAJ, FORWARDSTALL, BACKWARDTRAJ, BACKWARDSTALL,
+		RIGHTTRAJ, RIGHTSTALL, LEFTTRAJ, LEFTSTALL, COMPLETE
 	}
 	TestPhase testPhase = TestPhase.IDLE;
 	
@@ -32,6 +33,7 @@ public class DriveControllerTest extends Test{
 	private boolean hedWithinTolerance;
 	
 	public DriveControllerTest(DriveController dc, TorDrive drive){
+		setNumberOfSubtests(8);
 		controller = dc;
 		this.drive = drive;
 		forward = new LinearTrajectory(1.0);
@@ -42,8 +44,11 @@ public class DriveControllerTest extends Test{
 	
 	protected void setup(){
 		testPhase = TestPhase.BEGIN;
+		controller.setMotionProfilingActive();
 		posWithinTolerance = true;
 		velWithinTolerance = true;
+		hedWithinTolerance = true;
+		reset();
 	}
 
 	public void run(boolean execute) {
@@ -65,13 +70,11 @@ public class DriveControllerTest extends Test{
 		case IDLE:
 			break;
 		case BEGIN:
-			controller.hardware.resetEncoder();
 			System.out.println("Ready to test forward trajectory.");
 			System.out.println("> press A to execute the test.");
 			testPhase = TestPhase.FORWARDTRAJ;
 			break;
 		case FORWARDTRAJ:
-			controller.setMotionProfilingActive();
 			if(executeButton && !executeButtonLast){
 				testLinearTrajectory(forward);
 				testPhase = TestPhase.FORWARDSTALL;
@@ -79,53 +82,74 @@ public class DriveControllerTest extends Test{
 			break;
 		case FORWARDSTALL:
 			if(forward.isComplete()){
-				if(rightEncoderDirection && !leftEncoderDirection){
-					System.err.println("TEST FAILED (Forward: Encoder): Left encoder is not going the right direction.");
-				}
-				else if(!rightEncoderDirection && leftEncoderDirection){
-					System.err.println("TEST FAILED (Forward: Encoder): Right encoder is not going the right direction.");
-				}
-				else if(!rightEncoderDirection && !leftEncoderDirection){
-					System.err.println("TEST FAILED (Forward: Encoder): Both encoders are not going the right direction.");
-				}
-				else{
-					System.out.println("TEST PASSED (Forward: Encoder): Both encoders are going the right direction.");
-				}
-				if(!posWithinTolerance && velWithinTolerance){
-					System.err.println("TEST FAILED (Forward: Position): The position is not within the tolerance");
-				}
-				else if(posWithinTolerance && !velWithinTolerance){
-					System.err.println("TEST FAILED (Forward: Velocity): The velocity is not within the tolernace");
-				}
-				else if(!posWithinTolerance && !velWithinTolerance){
-					System.err.println("TEST FAILED (Forward: Pos&Vel): Both the position and the velocity is not within the tolernace");
-				}
-				else{
-					System.out.println("TEST PASSED (Forward: Translation): The position and velocity is within the tolerance.");
-				}
+				printEncoderError();
+				printLinearToleranceError();
+				reset();
+				System.out.println();
 				System.out.println("Ready to test backward trajectory.");
 				System.out.println("> press A to execute the test.");
-				testPhase = TestPhase.IDLE;
+				testPhase = TestPhase.BACKWARDTRAJ;
 			}
 			break;
 		case BACKWARDTRAJ:
 			if(executeButton && !executeButtonLast){
-				
+				testLinearTrajectory(backward);
+				testPhase = TestPhase.BACKWARDSTALL;
+			}
+			break;
+		case BACKWARDSTALL:
+			if(backward.isComplete()){
+				printEncoderError();
+				printLinearToleranceError();
+				reset();
+				System.out.println();
+				System.out.println("Ready to test right trajectory.");
+				System.out.println("> press A to execute the test.");
+				testPhase = TestPhase.RIGHTTRAJ;
 			}
 			break;
 		case RIGHTTRAJ:
+			if(executeButton && !executeButtonLast){
+				testPivotTrajectory(rightTurn);
+				testPhase = TestPhase.RIGHTSTALL;
+			}
+			break;
+		case RIGHTSTALL:
+			if(rightTurn.isComplete()){
+				printEncoderError();
+				printPivotToleranceError();
+				reset();
+				System.out.println();
+				System.out.println("Ready to test left trajectory.");
+				System.out.println("> press A to execute the test.");
+				testPhase = TestPhase.LEFTTRAJ;
+			}
 			break;
 		case LEFTTRAJ:
+			if(executeButton && !executeButtonLast){
+				testPivotTrajectory(leftTurn);
+				testPhase = TestPhase.LEFTSTALL;
+			}
+			break;
+		case LEFTSTALL:
+			if(leftTurn.isComplete()){
+				printEncoderError();
+				printPivotToleranceError();
+				reset();
+				System.out.println();
+				testPhase = TestPhase.COMPLETE;
+			}
 			break;
 		case COMPLETE:
+			System.out.println("Completed DriveController test. Result: " + result());
+			reset();
+			controller.setMotionProfilingInactive();
 			break;
 		}
 	}
-	
+
 	public void testLinearTrajectory(TorTrajectory traj){
-		//Execute trajectory
 		drive.executeTrajectory(traj);
-		//Check if the encoders are going the right direction
 		if(traj.getGoalPos() > 0){
 			if(controller.hardware.getRightEncoder() > 0){
 				rightEncoderDirection = true;
@@ -154,7 +178,6 @@ public class DriveControllerTest extends Test{
 				leftEncoderDirection = false;
 			}
 		}
-		//Compare the difference between target and current and see if its within the tolerance
 		if(Math.abs(controller.getPositionError()) >= controller.getPositionTolerance()){
 			posWithinTolerance = false;
 		}
@@ -162,48 +185,88 @@ public class DriveControllerTest extends Test{
 			velWithinTolerance = false;
 		}
 	}
-	
-	public void testPivotTrajectory(TorTrajectory traj){
-		//Execute trajectory
-				drive.executeTrajectory(traj);
-				//Check if the encoders are going the right direction
-				if(traj.getGoalHeading() > 0){
-					if(controller.hardware.getRightEncoder() < 0){
-						rightEncoderDirection = true;
-					}
-					else{
-						rightEncoderDirection = false;
-					}
-					if(controller.hardware.getLeftEncoder() > 0){
-						leftEncoderDirection = true;
-					}
-					else{
-						leftEncoderDirection = false;
-					}
-				}
-				else{
-					if(controller.hardware.getRightEncoder() > 0){
-						rightEncoderDirection = true;
-					}
-					else{
-						rightEncoderDirection = false;
-					}
-					if(controller.hardware.getLeftEncoder() < 0){
-						leftEncoderDirection = true;
-					}
-					else{
-						leftEncoderDirection = false;
-					}
-				}
-				//Compare the difference between target and current and see if its within the tolerance
-				if(Math.abs(controller.getHeadingError()) >= controller.getHeadingTolerance()){
-					hedWithinTolerance = false;
-				}
-	}
 
-	@Override
+	public void testPivotTrajectory(TorTrajectory traj){
+		drive.executeTrajectory(traj);
+		if(traj.getGoalHeading() > 0){
+			if(controller.hardware.getRightEncoder() < 0){
+				rightEncoderDirection = true;
+			}
+			else{
+				rightEncoderDirection = false;
+			}
+			if(controller.hardware.getLeftEncoder() > 0){
+				leftEncoderDirection = true;
+			}
+			else{
+				leftEncoderDirection = false;
+			}
+		}
+		else{
+			if(controller.hardware.getRightEncoder() > 0){
+				rightEncoderDirection = true;
+			}
+			else{
+				rightEncoderDirection = false;
+			}
+			if(controller.hardware.getLeftEncoder() < 0){
+				leftEncoderDirection = true;
+			}
+			else{
+				leftEncoderDirection = false;
+			}
+		}
+		if(Math.abs(controller.getHeadingError()) >= controller.getHeadingTolerance()){
+			hedWithinTolerance = false;
+		}
+	}
+	
+	public void printEncoderError(){
+		if(rightEncoderDirection && !leftEncoderDirection){
+			System.err.println("TEST FAILED (Encoder): Left encoder is not going the right direction.");
+		}
+		else if(!rightEncoderDirection && leftEncoderDirection){
+			System.err.println("TEST FAILED (Encoder): Right encoder is not going the right direction.");
+		}
+		else if(!rightEncoderDirection && !leftEncoderDirection){
+			System.err.println("TEST FAILED (Encoder): Both encoders are not going the right direction.");
+		}
+		else{
+			System.out.println("TEST PASSED (Encoder): Both encoders are going the right direction.");
+			incrementSubtestsPassed();
+		}
+	}
+	
+	public void printLinearToleranceError(){
+		if(!posWithinTolerance && velWithinTolerance){
+			System.err.println("TEST FAILED (Position): The position is not within the tolerance");
+		}
+		else if(posWithinTolerance && !velWithinTolerance){
+			System.err.println("TEST FAILED (Velocity): The velocity is not within the tolernace");
+		}
+		else if(!posWithinTolerance && !velWithinTolerance){
+			System.err.println("TEST FAILED (Pos&Vel): Both the position and the velocity is not within the tolernace");
+		}
+		else{
+			System.out.println("TEST PASSED (Translation): The position and velocity is within the tolerance.");
+			incrementSubtestsPassed();
+		}
+	}
+	
+	public void printPivotToleranceError(){
+		if(!hedWithinTolerance){
+			System.err.println("TEST FAILED (Heading): The heading is not within the tolerance");
+		}
+		else{
+			System.out.println("TEST PASSED (Rotation): The heading is within the tolerance");
+			incrementSubtestsPassed();
+		}
+	}
+	
 	protected void reset() {
-		// TODO Auto-generated method stub
-		
+		controller.hardware.resetEncoder();
+		posWithinTolerance = true;
+		velWithinTolerance = true;
+		hedWithinTolerance = true;
 	}
 }
